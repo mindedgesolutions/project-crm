b3db40f5690d71c8155f438bd4a0416618ce2a84
Souvik Nag <s.nag26@gmail.com>
2024-10-02 13:53:47 +0530

    new pages added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import * as Crm from "@/pages";
+
+const router = createBrowserRouter([
+  // Super admin routes starts ------
+  { path: `/admin/login`, element: <Crm.AdLogin /> },
+  { path: `/admin/forgot-password`, element: <Crm.AdForgotPassword /> },
+  { path: `/admin/reset-password`, element: <Crm.AdResetPassword /> },
+  {
+    path: `/admin`,
+    element: <Crm.AdLayout />,
+    children: [{ index: true, element: <Crm.AdDashboard /> }],
+  },
+  // Super admin routes ends ------
+]);
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/src/pages/admin/auth/AdLogin.tsx
@@ -0,0 +1,94 @@
+import loginBg from "@/assets/images/loginBg.svg";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Eye } from "lucide-react";
+import { useState } from "react";
+import { Form, Link } from "react-router-dom";
+
+const AdLogin = () => {
+  document.title = `Admin Login | ${import.meta.env.VITE_APP_TITLE}`;
+  const [type, setType] = useState("password");
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-4">
+      <div className="flex justify-end items-end">
+        <div className="w-full md:max-w-md p-6">
+          <div className="flex justify-center items-center mb-8">
+            <h3 className="text-5xl font-bold text-gray-800 tracking-widest">
+              CRM
+            </h3>
+          </div>
+          <Form>
+            <div className="flex flex-col space-y-4">
+              <div className="w-full items-center gap-1.5">
+                <Label
+                  htmlFor="username"
+                  className="capitalize tracking-widest"
+                >
+                  username
+                </Label>
+                <Input
+                  type="text"
+                  id="username"
+                  name="username"
+                  placeholder="johndoe@test.com"
+                />
+              </div>
+              <div className="w-full items-center gap-1.5">
+                <Label
+                  htmlFor="password"
+                  className="capitalize tracking-widest"
+                >
+                  password
+                </Label>
+                <div className="flex flex-row justify-center items-center relative">
+                  <Input
+                    type={type}
+                    id="password"
+                    name="password"
+                    placeholder="********"
+                  />
+                  <button
+                    type="button"
+                    className="absolute h-full right-0 px-2 hover:bg-muted"
+                    onClick={() =>
+                      setType(type === "password" ? "text" : "password")
+                    }
+                  >
+                    <Eye size={20} className="font-normal" />
+                  </button>
+                </div>
+              </div>
+              <div className="flex flex-row justify-end items-center">
+                <Link to={`/admin/forgot-password`}>
+                  <p className="text-sm tracking-widest text-gray-800 hover:text-gray-600">
+                    Forgot password?
+                  </p>
+                </Link>
+              </div>
+            </div>
+            <div className="flex justify-center items-center">
+              <Button
+                type="submit"
+                className="w-full uppercase tracking-widest my-8"
+              >
+                login
+              </Button>
+            </div>
+          </Form>
+        </div>
+      </div>
+      <div className="hidden md:flex flex-row h-screen justify-center items-center">
+        <img
+          src={loginBg}
+          alt={import.meta.env.VITE_APP_TITLE}
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+export default AdLogin;
+
+// Action function starts ------
